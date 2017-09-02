8e7d5ed9f288ca9b5cc322c8c19a41e0dd04ab7a
Default User <laurence.a.maddox@gmail.com>
2017-09-02 18:02:06 -0500

    added generic template support

--- cowsay.c
+++ cowsay.c
@@ -4,38 +4,36 @@
 
 typedef struct {
 	char const *cowsay;
-	char const *dashLength;
+	char *dashLength;
 	size_t lnlen;
 } cowsay_t;
 
-char *build_dashLength (size_t lnlen) {
+void build_dashLength (char *dl, size_t lnlen) {
+	/*
 	size_t i;
-	char *ret = malloc (lnlen + 1);
-	if (ret == NULL) return NULL;
 	for (i = 0; i != lnlen; i++)
-		ret[i] = '-';
-	ret[i] = '\0';
-	return ret;
+		dl[i] = '-';
+	dl[i] = '\0';
+	*/
+	memset (dl, (int) '-', lnlen);
+	dl[lnlen] = '\0';
 }
 
-int init_cowsay (cowsay_t *cowsay, char *cs) {
+int init_cowsay (cowsay_t *cowsay, char const *cs) {
 	cowsay->cowsay = cs;
 	cowsay->lnlen = strlen (cs);
-	cowsay->dashLength = build_dashLength (cowsay->lnlen);
+	cowsay->dashLength = malloc (cowsay->lnlen + 1);
 	if (cowsay->dashLength == NULL) return -1;
+	build_dashLength (cowsay->dashLength, cowsay->lnlen);
 	return 0;
 }
 
-char *build_cow (cowsay_t *cowsay) {
-	const char template[] = "--%s--\n"
-		"< %s >\n"
-		"--%s--\n"
-		"  |  ^__^\n"
-		"   - (oo)|_______\n"
-		"     (__)|       )/|/\n"
-		"         ||----w |\n"
-		"         ||     ||\n";
-	size_t tsz = sizeof (template) - 2 * 3 - 1;
+void free_cowsay (cowsay_t *cowsay) {
+	free (cowsay->dashLength);
+}
+
+char *build_cow (cowsay_t const *cowsay, char const *template) {
+	size_t tsz = strlen (template) - 2 * 3 ;
 	size_t outsz = tsz + cowsay->lnlen * 3;
 	char *out = malloc (outsz + 1);
 	if (outsz != snprintf (out, outsz, template,
@@ -51,8 +49,24 @@ char *build_cow (cowsay_t *cowsay) {
 
 int main(int argc, char* argv[]){
 	cowsay_t cs;
-	init_cowsay (&cs, "Hello, World!");
-	puts (build_cow (&cs));
-	/*return EXIT_SUCCESS;*/
-	return 0;
+	char *out;
+	if (init_cowsay (&cs, "Hello, World!") != 0)
+		return EXIT_FAILURE;
+	out = build_cow (&cs,
+		"--%s--\n"
+		"< %s >\n"
+		"--%s--\n"
+		"  |  ^__^\n"
+		"   - (oo)|_______\n"
+		"     (__)|       )/|/\n"
+		"         ||----w |\n"
+		"         ||     ||\n");
+	if (out == NULL) {
+		free_cowsay (&cs);
+		return EXIT_FAILURE;
+	}
+	puts (out);
+	free (out);
+	free_cowsay (&cs);
+	return EXIT_SUCCESS;
 }
