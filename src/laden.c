660598671eb2b93e2bb3c06ebece54507f770b0d
Default User <laurence.a.maddox@gmail.com>
2017-09-02 18:38:46 -0500

    added the legendary joke program, ported from Java

---
+++ src/laden.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "cowsay.h"
+
+int main (int argc, char *argv[]){
+	if (ezcowsay ("Durka Durka! Allah Akhbar! Durka Durka!",
+		"--%s--\n"
+		"< %s >\n"
+		"--%s--\n"
+		"       _,,--,,_    \n"
+		"     /`       .`\\  \n"
+		"    /  '  _.-'   \\ \n"
+		"    |  `'_{}_    | \n"
+		"    |  /`    `\\  | \n"
+		"     \\/ ==  == \\/  \n"
+		"     /| (.)(.) |\\  \n"
+		"     \\|  __)_  |/  \n"
+		"      |\\/____\\/|   \n"
+		"      | ` ~~ ` |   \n"
+		"      \\        /   \n"
+		"jgs    `.____.`    \n",
+		puts) != 0)
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}
