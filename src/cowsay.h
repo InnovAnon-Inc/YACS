eb0894fc81e09c3acabda999f8196b3a7093f31e
Default User <laurence.a.maddox@gmail.com>
2017-09-10 16:13:19 -0500

    adding warnings

--- src/cowsay.h
+++ src/cowsay.h
@@ -5,7 +5,9 @@
 extern "C" {
 #endif
 
-#include <stdlib.h>
+#include <sys/types.h>
+
+#include <glitter.h>
 
 typedef struct {
 	char const *cowsay;
@@ -16,22 +18,22 @@ typedef struct {
 void build_dashLength (char *dl, size_t lnlen)
 __attribute__ ((nonnull (1), nothrow)) ;
 
-void init_cowsay (cowsay_t *cowsay, char const *cs, char *dl, size_t lnlen)
+void init_cowsay (cowsay_t restrict* cowsay, char const restrict *cs, char restrict *dl, size_t lnlen)
 __attribute__ ((nonnull (1, 2, 3), nothrow)) ;
 
-int alloc_cowsay (cowsay_t *cowsay, char const *cs)
+int alloc_cowsay (cowsay_t restrict *cowsay, char const restrict *cs)
 __attribute__ ((nonnull (1, 2), nothrow, warn_unused_result)) ;
 
 void free_cowsay (cowsay_t *cowsay)
 __attribute__ ((nonnull (1), nothrow)) ;
 
-char *build_cow (cowsay_t const *cowsay, char const *template)
+char *build_cow (cowsay_t const restrict *cowsay, char const restrict *template)
 __attribute__ ((nonnull (1, 2), nothrow, warn_unused_result)) ;
 
 typedef __attribute__ ((nonnull (1), warn_unused_result))
 	int (*ezcowsaycb_t) (char const *) ;
 
-int ezcowsay (char const *str, char const *template,
+int ezcowsay (char const restrict *str, char const restrict *template,
 	ezcowsaycb_t cb)
 __attribute__ ((nonnull (1, 2, 3), warn_unused_result)) ;
 
